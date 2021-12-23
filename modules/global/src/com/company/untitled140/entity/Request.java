f141101e27044f63d42903b4b9ebc1e00409bba7
krasnovss91 <krasnovstanislav91@gmail.com>
2021-12-23 10:51:02 +0400

    Request_2

--- modules/global/src/com/company/untitled140/entity/Request.java
+++ modules/global/src/com/company/untitled140/entity/Request.java
@@ -17,14 +17,58 @@ import javax.persistence.*;
 public class Request extends TsCard {
     private static final long serialVersionUID = 6935893211447730139L;
 
-    @Column(name = "NUMBER_", length = 50)
-    protected String number;
+    @Column(name = "DESCRIPTIONS")
+    protected String descriptions;
 
-    public String getNumber() {
-        return number;
+    @Column(name = "JUSTIFICATION_FOR_EXCLUSIVITY")
+    protected String justificationForExclusivity;
+
+    @Column(name = "PURPOSE_OF_BUSINESS_TRIP")
+    protected String purposeOfBusinessTrip;
+
+    @Column(name = "WAY")
+    protected String way;
+
+    @Column(name = "NAME_OF_CARGO")
+    protected String nameOfCargo;
+
+    public String getNameOfCargo() {
+        return nameOfCargo;
     }
 
-    public void setNumber(String number) {
-        this.number = number;
+    public void setNameOfCargo(String nameOfCargo) {
+        this.nameOfCargo = nameOfCargo;
+    }
+
+    public String getWay() {
+        return way;
+    }
+
+    public void setWay(String way) {
+        this.way = way;
+    }
+
+    public String getPurposeOfBusinessTrip() {
+        return purposeOfBusinessTrip;
+    }
+
+    public void setPurposeOfBusinessTrip(String purposeOfBusinessTrip) {
+        this.purposeOfBusinessTrip = purposeOfBusinessTrip;
+    }
+
+    public String getJustificationForExclusivity() {
+        return justificationForExclusivity;
+    }
+
+    public void setJustificationForExclusivity(String justificationForExclusivity) {
+        this.justificationForExclusivity = justificationForExclusivity;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.descriptions = descriptions;
     }
 }
